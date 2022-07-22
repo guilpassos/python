9a74ce252d39cbfcf15ae42107f59c41f6f80481
guilpassos <guilpassos@protonmail.com>
2022-07-22 18:36:04 -0300

    ex010

---
+++ .history/python_exercicios/ex010_20220722183502.py
@@ -0,0 +1,3 @@
+real = float(input('Quanto dinheiro voce tem na carteira? R$'))
+dolar = real/5.49
+print('Com R${} voce consegue comprar US${}'.format(real, dolar))
---
+++ .history/python_exercicios/ex010_20220722183540.py
@@ -0,0 +1,3 @@
+real = float(input('Quanto dinheiro voce tem na carteira? R$'))
+dolar = real/5.49
+print('Com R${:.2f} voce consegue comprar US${:.2f}'.format(real, dolar))
