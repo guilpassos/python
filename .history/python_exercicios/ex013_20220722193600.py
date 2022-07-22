555cce8651f7e019e68c7b5d8fdea024eb446705
guilpassos <guilpassos@protonmail.com>
2022-07-22 19:36:44 -0300

    ex013

---
+++ .history/python_exercicios/ex013_20220722193600.py
@@ -0,0 +1,4 @@
+salario = float(input('Qual o salário do funcionário? R$'))
+novo = salario + salario*(15/100)
+print('O salário do funcionário é {:.2f} mas com o aumento de 15% vai ser {:.2f}'.format(salario, novo))
+
