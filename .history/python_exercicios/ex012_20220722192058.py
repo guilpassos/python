63b36ef486138614b35a8a03ae30139a34efa3ec
guilpassos <guilpassos@protonmail.com>
2022-07-22 19:24:14 -0300

    ex012

---
+++ .history/python_exercicios/ex012_20220722192058.py
@@ -0,0 +1,2 @@
+p = float(input('Qual o preco do produto? '))
+print('O produto que custava R${:.2} na promocao com desconto de 5% vai custar R${}'.format(p, p*0.95))
---
+++ .history/python_exercicios/ex012_20220722192106.py
@@ -0,0 +1,2 @@
+p = float(input('Qual o preco do produto? '))
+print('O produto que custava R${:.2f} na promocao com desconto de 5% vai custar R${:.2f}'.format(p, p*0.95))
