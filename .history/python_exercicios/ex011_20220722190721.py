fcb6566135b0e9361ef36205bc6dea1ead9a2190
guilpassos <guilpassos@protonmail.com>
2022-07-22 19:08:27 -0300

    ex011

---
+++ .history/python_exercicios/ex011_20220722190721.py
@@ -0,0 +1,4 @@
+l = float(input('Largura da parede: '))
+h = float(input('Altura da parede'))
+print('Sua parede tem dimensao de {}x{} e sua área é de {}m²'.format(l, h, l*h))
+print('Para pintar sua parede, voce precisará de {}L de tinta'.format((l*h)/2))
