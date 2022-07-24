c49f274fc0a838651bfde90353e8b2c4ef87a005
guilpassos <guilpassos@protonmail.com>
2022-07-24 15:37:42 -0300

    ex019

---
+++ .history/python_exercicios/ex019_20220724153555.py
@@ -0,0 +1,7 @@
+import random
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+lista = []
+print('O escolhido foi {}'.format
