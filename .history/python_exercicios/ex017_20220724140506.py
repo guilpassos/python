81cbe09bbfc3ea64e02d58bc69adfd05e07b6346
guilpassos <guilpassos@protonmail.com>
2022-07-24 14:05:58 -0300

    ex017

---
+++ .history/python_exercicios/ex017_20220724140506.py
@@ -0,0 +1,8 @@
+from math import sqrt
+co = float(input('Qual o cateto oposto: '))
+ca = float(input('Qual o cateto adjacente: '))
+h1 = co **2+ca**2
+h2 = sqrt(h2)
+print('A hipotenusa do trinagulo vale {}'.format(h2))
+
+ 
---
+++ .history/python_exercicios/ex017_20220724140535.py
@@ -0,0 +1,9 @@
+from math import sqrt
+co = float(input('Qual o cateto oposto: '))
+ca = float(input('Qual o cateto adjacente: '))
+h1 = co ** 2 + ca ** 2
+h2 = sqrt(h1)
+print('A hipotenusa do trinagulo vale {}'.format(h2))
+
+
+ 
