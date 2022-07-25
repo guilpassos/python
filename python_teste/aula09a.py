78922dd88003ceb5436d49759d256c6bca1d2e20
guilpassos <guilpassos@protonmail.com>
2022-07-25 17:39:27 -0300

    aula09a

---
+++ python_teste/aula09a.py
@@ -0,0 +1,4 @@
+frase = ('Curso em video Python')
+print(frase)
+
+print("""Nessa aula, vamos aprender operações com String no Python. As principais operações que vamos aprender são o Fatiamento de String, Análise com len(), count(), find(), transformações com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().""")
