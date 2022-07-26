c6b4e21a8cf6c394f81a4bc22a36c0ba7b81c65c
guilpassos <guilpassos@protonmail.com>
2022-07-26 20:17:21 -0300

    aula10b

---
+++ python_teste/aula10b.py
@@ -0,0 +1,9 @@
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = (n1+n2)/2
+print('A sua média foi {}'.format(media))
+if media>=6:
+    print('A sua média foi boa!')
+else:
+    print('A sua média foi ruim. Voce precisa estudar mais!')
+print('Tenha um bom dia!')
