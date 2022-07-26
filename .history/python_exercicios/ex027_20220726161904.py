809ed84478266cebde5ccf8e3e48b86396ddda3f
guilpassos <guilpassos@protonmail.com>
2022-07-26 16:20:28 -0300

    ex027

---
+++ .history/python_exercicios/ex027_20220726161904.py
@@ -0,0 +1,17 @@
+n = str(input('Digite seu nome completo: ')).strip()
+nome = n.split()
+print('Prazer em te conhecer!')
+print('Seu primeiro nome: {}'.format(nome[0]))
+print('Seu ultimo nome: {}'.format(len(nome)))
+
+
+
+
+
+
+
+
+
+
+#Exercício Python 27: Faça um programa que leia o nome completo de uma pessoa,
+# mostrando em seguida o primeiro e o último nome separadamente.
