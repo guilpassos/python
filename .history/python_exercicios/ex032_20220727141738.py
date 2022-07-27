1f810c96f0f851e0cdd5ae929eecb508e45088a1
guilpassos <guilpassos@protonmail.com>
2022-07-27 14:21:30 -0300

    ex032

---
+++ .history/python_exercicios/ex032_20220727141738.py
@@ -0,0 +1,17 @@
+ano = int(input('Qual ano deseja analisar? '))
+if ano % 4 ==0:
+    print('O ano {} é BISSEXTO!'.format(ano))
+else:
+    print('O ano {} nao é BISSEXTO!'.format(ano))
+    
+
+
+
+
+
+
+
+
+
+#Exercício Python 32: Faça um programa que leia
+# um ano qualquer e mostre se ele é bissexto.
---
+++ .history/python_exercicios/ex032_20220727141951.py
@@ -0,0 +1,19 @@
+from datetime import date
+ano = int(input('Qual ano deseja analisar? Coloque 0 para analisar o ano atual: '))
+if ano =
+if ano % 4 ==0 and ano % 100 != 0 or ano % 400 ==0:
+    print('O ano {} é BISSEXTO!'.format(ano))
+else:
+    print('O ano {} nao é BISSEXTO!'.format(ano))
+    
+
+
+
+
+
+
+
+
+
+#Exercício Python 32: Faça um programa que leia
+# um ano qualquer e mostre se ele é bissexto.
