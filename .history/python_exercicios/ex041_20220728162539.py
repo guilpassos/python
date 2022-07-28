3853a58370228a446829971ed1f1359ef0902d87
guilpassos <guilpassos@protonmail.com>
2022-07-28 16:25:54 -0300

    ex041

---
+++ .history/python_exercicios/ex041_20220728162539.py
@@ -0,0 +1,35 @@
+from datetime import date
+nome = str(input('Nome do atleta: '))
+ano = int(input('Ano de nascimento: '))
+atual = date.today().year
+idade = atual - ano
+if idade <= 9:
+    print('O atleta {}, nasceu em {} e tem {} anos.'.format(nome, ano, idade))
+    print('CATEGORIA MIRIM')
+elif idade > 9 and idade <= 14:
+    print('O atleta {}, nasceu em {} e tem {} anos.'.format(nome, ano, idade))
+    print('CATEGORIA INFANTIL')
+elif idade > 14 and idade <= 19:
+    print('O atleta {}, nasceu em {} e tem {} anos.'.format(nome, ano, idade))
+    print('CATEGORIA JUNIOR')
+elif idade > 19 and idade <= 25:
+    print('O atleta {}, nasceu em {} e tem {} anos.'.format(nome, ano, idade))
+    print('CATEGORIA SENIOR')
+elif idade > 25:
+    print('O atleta {}, nasceu em {} e tem {} anos.'.format(nome, ano, idade))
+    print('CATEGORIA MASTER')
+
+
+#Exercício Python 041: A Confederação Nacional de Natação precisa
+# de um programa que leia o ano de nascimento de um atleta
+# e mostre sua categoria, de acordo com a idade:
+
+#– Até 9 anos: MIRIM
+
+#– Até 14 anos: INFANTIL
+
+#– Até 19 anos: JÚNIOR
+
+#– Até 25 anos: SÊNIOR
+
+#– Acima de 25 anos: MASTER
