d79d9a5067ef0e353438bc5e2fa4a29e20ed5231
guilpassos <guilpassos@protonmail.com>
2022-07-28 15:06:08 -0300

    ex038

---
+++ .history/python_exercicios/ex038_20220728150544.py
@@ -0,0 +1,23 @@
+a = int(input('Primeiro número: '))
+b = int(input('Segundo número: '))
+if a > b:
+    print('O PRIMEIRO valor é maior!')
+elif b > a:
+    print('O SEGUNDO valor é maior!')
+else:
+    print('Os dois valores sao IGUAIS!')
+
+
+
+
+
+
+
+#Exercício Python 038: Escreva um programa que leia dois
+# números inteiros e compare-os. mostrando
+# na tela uma mensagem:
+– O primeiro valor é maior
+
+– O segundo valor é maior
+
+– Não existe valor maior, os dois são iguais
