37b8d23c3cdfded87976f7800604081279ed12cc
guilpassos <guilpassos@protonmail.com>
2022-08-09 11:01:34 -0300

    ex067

---
+++ .history/python_exercicios/ex067_20220809110051.py
@@ -0,0 +1,15 @@
+while True:
+    n = int(input('Quer ver a tabuada de qual valor? '))
+    if n < 0:
+        break
+    print('-'*40)
+    for c in range(1,11):
+        p = n * c
+        print(f'{n}  x  {c}  =  {p}')
+        c += 1
+    print('-'*40)
+print('Programa encerrado!')
+
+#Exercício Python 67: Faça um programa que mostre a tabuada de vários números,
+# um de cada vez, para cada valor digitado pelo usuário. O programa será
+# interrompido quando o número solicitado for negativo.
