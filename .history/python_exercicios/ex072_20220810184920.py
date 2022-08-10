aaf1549f7e7a09f10c8bc365323896bcf7385cfc
guilpassos <guilpassos@protonmail.com>
2022-08-10 18:51:30 -0300

    ex072

---
+++ .history/python_exercicios/ex072_20220810184920.py
@@ -0,0 +1,20 @@
+cont = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito',
+        'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis',
+        'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        print('O número digitado foi', cont[num])
+        pergunta = str(input('Deseja continuar? [S/N] ')).strip()[0].upper()
+            if pergunta in 'N':
+                break
+
+        
+    print('Tente novamente! ', end='')
+print('O número digitado foi', cont[num])
+
+
+#Exercício Python 72: Crie um programa que tenha uma dupla totalmente 
+# preenchida com uma contagem por extenso, de zero até vinte.
+# Seu programa deverá ler um número pelo teclado (entre 0 e 20)
+# e mostrá-lo por extenso.
---
+++ .history/python_exercicios/ex072_20220810185106.py
@@ -0,0 +1,19 @@
+cont = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito',
+        'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis',
+        'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        print('O número digitado foi', cont[num])
+        pergunta = str(input('Deseja continuar? [S/N] ')).strip()[0].upper()
+        if pergunta in 'N':
+            break
+print('Volte sempre!20')
+        
+  
+
+
+#Exercício Python 72: Crie um programa que tenha uma dupla totalmente 
+# preenchida com uma contagem por extenso, de zero até vinte.
+# Seu programa deverá ler um número pelo teclado (entre 0 e 20)
+# e mostrá-lo por extenso.
