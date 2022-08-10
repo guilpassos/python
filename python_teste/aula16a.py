1f0ee628a1fe93b09f08f89aa9b7173406a25ce2
guilpassos <guilpassos@protonmail.com>
2022-08-10 16:01:56 -0300

    aula16a

---
+++ python_teste/aula16a.py
@@ -0,0 +1,11 @@
+lanche = ('hamburguer', 'suco', 'pizza', 'pudim', 'batata frita')
+
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]}')
+
+#for comida in lanche:
+#    print(f'Eu vou comer {comida}.')
+
+
+# lanche é uma TUPLA (tuplas sao variaveis compostas e imutaveis)
+# print(len(lanche))
