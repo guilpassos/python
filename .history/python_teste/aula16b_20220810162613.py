67ab8100cf322bc4a90133fe0f340286b7f17274
guilpassos <guilpassos@protonmail.com>
2022-08-10 16:26:20 -0300

    aula16b

---
+++ .history/python_teste/aula16b_20220810162613.py
@@ -0,0 +1,19 @@
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+print(c)
+# print(c.index(5, 1))
+# mostra a posicao em que o elemento esta, e apos a virgula indica a partir de que posicao comeca a contar
+
+#print(c.index(2))
+# mostra a posicao em que o elemento esta
+
+# print(c.count(5))
+# mostra quantas vezes o elemento(numero) aparece na tupla
+
+# print(len(c)
+# mostra a extensao da tupla
+
+#lanche = ('hamburguer', 'suco', 'pizza', 'pudim', 'batata frita')
+#print(sorted(lanche))
+# O comando sorted coloca a TUPLA em ordem
