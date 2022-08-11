3deb9f02f5d2e7e628060487990838aa56d2ea04
guilpassos <guilpassos@protonmail.com>
2022-08-11 15:23:16 -0300

    ex073

---
+++ .history/python_exercicios/ex073_20220811152219.py
@@ -0,0 +1,28 @@
+times = ('Corinthians', 'Palmeiras', 'Santos', 'Gremio', 'Cuzeiro', 'Flamengo', 'Vasco da Gama',
+               'Chapecoense', 'Atlético', 'Botafogo', 'Atlético- PR', 'Bahia', 'São Paulo', 'Fluminense',
+               'Sport Recife', 'EC Vitória', 'Coritiba', 'Avaí', 'Ponte Preta', 'Atlético-GO')
+print('=' * 40)
+print('Os times do brasileirão são: ', times)
+print('=' * 40)
+print('Os 5 primeiros colocados são: ', times[0:5])
+print('=' * 40)
+print('Os últimos 4 colocados são: ', times[-4:])
+print('=' * 40)
+print('Times em ordem alfabética: ', sorted(times))
+print('=' * 40)
+print(f'O chapecoense se encontra na {times.index("Chapecoense")} posicão.')
+print('=' * 40)
+
+
+
+#Exercício Python 73: Crie uma tupla preenchida com os 20 primeiros
+# colocados da Tabela do Campeonato Brasileiro de Futebol,
+# na ordem de colocação. Depois mostre:
+
+#a) Os 5 primeiros times.
+
+#b) Os últimos 4 colocados.
+
+#c) Times em ordem alfabética.
+
+#d) Em que posição está o time da Chapecoense.
