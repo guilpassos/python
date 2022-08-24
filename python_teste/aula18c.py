81d9508338f6eda32c9b8d11a3ffdb0567009170
guilpassos <guilpassos@protonmail.com>
2022-08-24 17:09:36 -0300

    aula18c

---
+++ python_teste/aula18c.py
@@ -0,0 +1,18 @@
+galera = list()
+dado = list()
+totma = totme = 0
+for c in range(0, 3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])
+    dado.clear()
+
+for p in galera:
+    if p[1] >= 21:
+        print(f'{p[0]} é maior de idade.')
+        totma += 1
+    else:
+        print(f'{p[0]} é menor de idade.')
+        totme += 1
+
+print(f'Temos {totma} maiores de idade e {totme} menores de idade.')
