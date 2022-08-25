b27fb7bf50375cedcd0b2a0c7867a0e33e3fca08
guilpassos <guilpassos@protonmail.com>
2022-08-25 15:33:31 -0300

    ex087

---
+++ .history/python_exercicios/ex087_20220825152720.py
@@ -0,0 +1,18 @@
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para {[l], [c]}: '))
+print('-=' * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]}]', end='')
+
+
+
+
+
+
+#Exercício Python 087: Aprimore o desafio anterior, mostrando no final:
+# A) A soma de todos os valores pares digitados.
+# B) A soma dos valores da terceira coluna.
+# C) O maior valor da segunda linha.
---
+++ .history/python_exercicios/ex087_20220825153025.py
@@ -0,0 +1,23 @@
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para {[l], [c]}: '))
+print('-=' * 30)
+print('A matriz para os valores digitados é:')
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+        if matriz[l][c] % 2 == 0:
+            
+
+    print()
+
+
+
+
+
+
+#Exercício Python 087: Aprimore o desafio anterior, mostrando no final:
+# A) A soma de todos os valores pares digitados.
+# B) A soma dos valores da terceira coluna.
+# C) O maior valor da segunda linha.
