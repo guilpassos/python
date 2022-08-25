1dde32e0515fe391b5dab29e6d1064afeac64d19
guilpassos <guilpassos@protonmail.com>
2022-08-25 16:34:33 -0300

    ex089

---
+++ .history/python_exercicios/ex089_20220825163144.py
@@ -0,0 +1,20 @@
+ficha = list()
+
+nome = str(input('Nome: '))
+nota1 = float(input('Nota 1: '))
+nota2 = float(0)
+
+
+
+
+
+
+
+
+
+
+
+#Exercício Python 089: Crie um programa que leia nome e duas notas
+# de vários alunos e guarde tudo em uma lista composta. No final,
+# mostre um boletim contendo a média de cada um e permita que
+# o usuário possa mostrar as notas de cada aluno individualmente.
---
+++ .history/python_exercicios/ex089_20220825163243.py
@@ -0,0 +1,21 @@
+ficha = list()
+
+nome = str(input('Nome: '))
+nota1 = float(input('Nota 1: '))
+nota2 = float(input('Nota 2: '))
+media = (nota1 + nota2) / 2
+
+ficha.append
+
+
+
+
+
+
+
+
+
+#Exercício Python 089: Crie um programa que leia nome e duas notas
+# de vários alunos e guarde tudo em uma lista composta. No final,
+# mostre um boletim contendo a média de cada um e permita que
+# o usuário possa mostrar as notas de cada aluno individualmente.
