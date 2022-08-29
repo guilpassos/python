fafbbdf51af71631a09dbd4265ce081804b5852c
guilpassos <guilpassos@protonmail.com>
2022-08-29 16:43:01 -0300

    ex094

---
+++ .history/python_exercicios/ex094_20220829163721.py
@@ -0,0 +1,23 @@
+pessoa = dict()
+
+pessoa['nome'] = str(input('Nome: '))
+while True:
+    pessoa['sexo'] = str(input('Sexo: ')).upper()[0]
+    if pessoa["sexo"]
+
+
+
+
+
+
+
+
+
+
+#Exercício Python 094: Crie um programa que leia nome, sexo e idade de várias pessoas,
+# guardando os dados de cada pessoa em um dicionário e todos os dicionários
+# em uma lista. No final, mostre: 
+# A) Quantas pessoas foram cadastradas 
+# B) A média de idade 
+# C) Uma lista com as mulheres 
+# D) Uma lista de pessoas com idade acima da média
