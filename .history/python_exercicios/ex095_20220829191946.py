c025b57fd14a440268336e5c39733d9b397f93ea
guilpassos <guilpassos@protonmail.com>
2022-08-29 19:41:40 -0300

    ex095

---
+++ .history/python_exercicios/ex095_20220829191946.py
@@ -0,0 +1,20 @@
+jogador = dict()
+partidas = list()
+jogador['nome'] = str(input('Nome do jogador: '))
+tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+for c in range(0, tot):
+    partidas.append(int(input(f'    Quantos gols na partida {c}? ')))
+jogador['gols'] = partidas[:]
+jogador['total'] = sum(partidas)
+
+print('=-' * 30)
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
+for i, v in enumerate(jogador['gols']):
+    print(f'   => Na partida {i} fez {v} gols.')
+print(f'Foi um total de {jogador["total"]} gols.')
+
+
+#Exercício Python 095: Aprimore o desafio 93 para que ele funcione
+# com vários jogadores, incluindo um sistema de visualização 
+# de detalhes do aproveitamento de cada jogador.
+
---
+++ .history/python_exercicios/ex095_20220829193109.py
@@ -0,0 +1,45 @@
+time = list()
+jogador = dict()
+partidas = list()
+
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do jogador: '))
+    tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    for c in range(0, tot):
+        partidas.append(int(input(f'    Quantos gols na partida {c}? ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break
+print('-' * 40)
+for k, v in enumerate(time):
+    print(f'{k:>3}', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-' * 40)
+
+
+
+
+
+
+
+print('=-' * 30)
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
+for i, v in enumerate(jogador['gols']):
+    print(f'   => Na partida {i} fez {v} gols.')
+print(f'Foi um total de {jogador["total"]} gols.')
+
+
+#Exercício Python 095: Aprimore o desafio 93 para que ele funcione
+# com vários jogadores, incluindo um sistema de visualização 
+# de detalhes do aproveitamento de cada jogador.
+
