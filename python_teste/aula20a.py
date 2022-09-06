62a769f49839ebb9abafe1ce5b77ba77e9e19439
guilpassos <guilpassos@protonmail.com>
2022-09-06 10:56:15 -0300

    aula20a

---
+++ python_teste/aula20a.py
@@ -0,0 +1,16 @@
+def lin():
+    print('-' * 30)
+
+#programa principal
+lin()
+print('FUNCAO SOMA')
+lin()
+
+def soma(a, b):
+    print(f'A = {a} e B = {b}')
+    s = a + b
+    print(f'A soma A + B = {s}')
+
+
+#programa principal
+soma(9, 1)
