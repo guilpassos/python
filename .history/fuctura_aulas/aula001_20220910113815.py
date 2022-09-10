9d54e74b9b1711e0fd5ab2330fb493157fe00131
guilpassos <guilpassos@protonmail.com>
2022-09-10 11:38:49 -0300

    aula001

---
+++ .history/fuctura_aulas/aula001_20220910113815.py
@@ -0,0 +1,7 @@
+cupom = str(input('Digite o cupom: ')).upper()
+if cupom == 'FUCTURA1':
+    print('Voce ganhou 15% de desconto!')
+elif cupom == 'FUCTURA2':
+    print('Voce ganhou 10% de desconto!')
+else:
+    print('Voce ganhou 5% de desconto!')
