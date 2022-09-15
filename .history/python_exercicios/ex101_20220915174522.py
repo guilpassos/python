39e378fce296c6df443872003ebfa091b72fac0c
guilpassos <guilpassos@protonmail.com>
2022-09-15 17:48:42 -0300

    ex101

---
+++ .history/python_exercicios/ex101_20220915174522.py
@@ -0,0 +1,20 @@
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: NAO VOTA.
+    else
+
+
+
+
+
+
+
+
+
+#Exercício Python 101: Crie um programa que tenha uma função chamada voto()
+# que vai receber como parâmetro o ano de nascimento de uma pessoa, 
+# retornando um valor literal indicando se uma pessoa tem voto 
+# NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
---
+++ .history/python_exercicios/ex101_20220915174752.py
@@ -0,0 +1,21 @@
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: NAO VOTA.
+    elif 16 <= idade < 18 or idade > 65:
+        return 
+
+
+
+
+
+
+
+
+
+#Exercício Python 101: Crie um programa que tenha uma função chamada voto()
+# que vai receber como parâmetro o ano de nascimento de uma pessoa, 
+# retornando um valor literal indicando se uma pessoa tem voto 
+# NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
