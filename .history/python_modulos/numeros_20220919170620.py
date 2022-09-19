367882026c56aa597c21151eaaf024d787688036
guilpassos <guilpassos@protonmail.com>
2022-09-19 17:06:35 -0300

    numeros

---
+++ .history/python_modulos/numeros_20220919170620.py
@@ -0,0 +1,7 @@
+import uteis
+#from uteis import fatorial, dobro
+
+num = int(input('Digite um valor: '))
+fat = uteis.fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+print(f'O dobro de {num} vale {uteis.dobro(num)}')
